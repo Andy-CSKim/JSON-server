a7ec3e599069181005d0944dcfa16ef9459f58ba
Andy-CSKim <andy.cskim@gmail.com>
2023-11-05 21:44:22 +0900

    add post/put/delete

--- axios-01/src/App.js
+++ axios-01/src/App.js
@@ -1,24 +1,66 @@
+
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
-import {getUser} from './fetch';
+import {getUser, postUser, putUser, deleteUser} from './fetch';
+
 
 
 function App() {
-  const [username, setUsername] = useState([]) // empty array
-  const handleRequest = () => {
-    console.log("sending request");
+  const [dataFromServer, setDataFromServer] = useState() // empty array
+
+  const doGet = () => {
+    console.log("Get()");
     // const resp = getUser();
     // console.log(resp);  // Promise
 
     getUser().then((resp) => {
       console.log(resp);
       // [0] : {id: 1, name: 'Lee', role: 'developer'}
-      setUsername(resp);
+      setDataFromServer(resp);
     });
 
   }
 
+  const doPost = () => {
+    console.log("Post()");
+    // const resp = getUser();
+    // console.log(resp);  // Promise
+    const newUser = {name: 'John', role: 'admin'};
+
+    postUser(newUser).then((resp) => {
+      console.log(resp);
+      // [0] : {id: 1, name: 'Lee', role: 'developer'}
+      setDataFromServer(resp);
+    });
+  }
+
+  const doPut = () => {
+    console.log("Put()");
+    // const resp = getUser();
+    // console.log(resp);  // Promise
+    const curUser = {name: 'John', role: 'admin'};
+    const id = 6;
+
+    putUser(id, curUser).then((resp) => {
+      console.log(resp);
+      // [0] : {id: 1, name: 'Lee', role: 'developer'}
+      setDataFromServer(resp);
+    });
+  }
+
+  const doDelete = () => {
+    console.log("Delete()");
+    // const resp = getUser();
+    // console.log(resp);  // Promise
+    const id = 6;
+
+    deleteUser(id).then((resp) => {
+      console.log(resp);
+      // [0] : {id: 1, name: 'Lee', role: 'developer'}
+      setDataFromServer(resp);
+    });
+  }  
   // username = []
   // username && username[0].name
   // <p>welcome {username.length !== 0 && username[0].name}</p>
@@ -29,9 +71,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Blockiki</p>
-        <button onClick={handleRequest}>Send Request</button>
-        {username.map((user) => <p>welcome {user.name}</p>)}
+        <p>CRUD example</p>
+        <button onClick={doGet}>Get</button> <br/>
+        <button onClick={doPost}>Post</button> <br/>
+        <button onClick={doPut}>Put</button> <br/>
+        <button onClick={doDelete}>Delete</button> <br/>
+
+        <br/><br/>
+        {/* {username.map((user) => <p>id:{user.id}<br/>name: {user.name}<br/> role: {user.role}</p>)} */}
+        <textarea name="postContent" rows={20} cols={100} value={dataFromServer}> </textarea>
+
       </header>
     </div>
   );
--- axios-01/src/fetch.js
+++ axios-01/src/fetch.js
@@ -20,3 +20,41 @@ export const getUser = async () => {
     return null;
   }
 };
+
+// newUser = {name: 'John', role: 'admin'}
+export const postUser = async (newUser) => {
+  //const response = await authApi.get<QueryResponse>('query');
+  try {
+    const response = await authApi.post('/users', newUser);  // http://localhost:3100/users
+    //console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+// newUser = {name: 'John', role: 'admin'}
+export const putUser = async (id, newUser) => {
+  //const response = await authApi.get<QueryResponse>('query');
+  try {
+    const response = await authApi.put(`/users/${id}`, newUser);  // http://localhost:3100/users/6
+    //console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const deleteUser = async (id) => {
+  //const response = await authApi.get<QueryResponse>('query');
+  try {
+    const response = await authApi.delete(`/users/${id}`);  // http://localhost:3100/users/6
+    //console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
