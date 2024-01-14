bf3475b8169ff3578091875454852ef54170b3f2
Andy-CSKim <andy.cskim@gmail.com>
2024-01-14 20:31:09 +0900

    add comment

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# localhost:3001/
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
+# localhost:3001/items/5?q=hello
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
