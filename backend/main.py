2124391844de67a3da6d4e334c37a023c80cb19c
SJ100810 <seungjune.yoon@gmail.com>
2024-01-14 20:42:56 +0800

    .

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+        name: str
+        price: float
 
 # localhost:3001/
 @app.get("/")
@@ -17,4 +21,5 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.post("/items")  # item = {"name": "Foo", "price": 42.0}
-def create_item():
+def create_item(item: Item):
+    return {"name": item.name, "price":item.price}
