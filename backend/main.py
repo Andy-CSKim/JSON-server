04d6c50a14abad99623250fc6bc7d1ffc5087916
SJ100810 <seungjune.yoon@gmail.com>
2024-01-14 21:00:45 +0800

    .

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,62 @@
 from typing import Union
-
+import random 
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 app = FastAPI()
+words = ['python', 'java', 'kotlin', 'javascript']
+CHOSENWORD = None
+wrongCount = 0
+answer = None
+ANSWER = None
 
-class Item(BaseModel):
-        name: str
-        price: float
+@app.get("/start") 
+def start_hangman():
+    global CHOSENWORD
+    global ANSWER
+    global answer
+
+    CHOSENWORD = list(words[random.randint(0, len(words)-1)])
+    answer = "_" * (len("".join(CHOSENWORD))) 
+    ANSWER = list(answer)
+    return ' '.join(ANSWER)
+
+
+def hangman(guess):
+    global CHOSENWORD
+    global answer 
+    global ANSWER
+    global wrongCount
+
+    count = 0
+    right = 0
+    while count < len(CHOSENWORD):
+        if guess == CHOSENWORD[count]:
+            ANSWER[count] = guess 
+            right += 1
+        count += 1
+    if right == 0:
+        wrongCount += 1
+        print (str(wrongCount) + " wrong guesses!")
+
+    answer = ' '.join(ANSWER)
+
+    return answer
+
+res = start_hangman()
+print(res)
+
+while True:
+    ch = input()
+    if ch != "":
+        print(hangman(ch))
+    if wrongCount > 6:
+        print("You are hanged!")
+        break
+    if ANSWER == CHOSENWORD:
+        print ("Congratulations!")
+        break
+
+
 
 # localhost:3001/
 @app.get("/")
